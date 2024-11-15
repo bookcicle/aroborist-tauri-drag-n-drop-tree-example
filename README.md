f4fc83c4db56bf2bd7292aca40fe763eca26cfe5
Andrew Barraford <andy.barraford@gmail.com>
2024-11-15 08:56:57 +0100

    init

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Tauri + React
+# Tauri + React + React-Arborist
 
-This template should help get you started developing with Tauri and React in Vite.
-
-## Recommended IDE Setup
-
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+This is a demo of using React-Arborist With Tauri, to tree features such as ordering across macos, windows, linux.
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,30 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import {Tree} from "react-arborist";
+
+const data = [
+    { id: "1", name: "Unread" },
+    { id: "2", name: "Threads" },
+    {
+        id: "3",
+        name: "Chat Rooms",
+        children: [
+            { id: "c1", name: "General" },
+            { id: "c2", name: "Random" },
+            { id: "c3", name: "Open Source Projects" },
+        ],
+    },
+    {
+        id: "4",
+        name: "Direct Messages",
+        children: [
+            { id: "d1", name: "Alice" },
+            { id: "d2", name: "Bob" },
+            { id: "d3", name: "Charlie" },
+        ],
+    },
+];
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -14,36 +38,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+      <Tree initialData={data} />;
     </main>
   );
 }
